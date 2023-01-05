3b3cbe04507cde96dbbe555caf48c8bd490a4e8b
tchomysz <tomaschomyszyn@gmail.com>
2023-01-05 09:29:50 -0300

    useTodos agregado

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from "react";
-import { todoReducer } from "../08-intro-reducer/todoReducer";
+import { todoReducer } from "./todoReducer";
 
 const initialState = [];
 
